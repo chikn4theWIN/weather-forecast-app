0af167b80980fba308693083fc58c3d7e545aefa
db <jacboysent@gmail.com>
2022-09-22 23:00:20 -0500

    finished last of JavaScript and cleaned up code a bit

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,7 +28,7 @@ var mainCard = $(".card-body");
 getItems();
 // Main card.
 function getData() { 
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=3e899aecdec7294393c72ce659cd2b48" 
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=49fb27317373bb54f7d9243387af6df3" 
     mainCard.empty();
     $("#weeklyForecast").empty();
     // Request the API.
@@ -37,7 +37,7 @@ function getData() {
         method: "GET"
         // Response function.
     }).then(function (response) {
-        var date = moment().format(" MM/DD/YYYY");
+        var date = moment().format(" (MM/DD/YYYY) ");
         
         var iconCode = response.weather[0].icon;
     
@@ -52,22 +52,22 @@ function getData() {
         var temp = Math.round((response.main.temp - 273.15) * 1.80 + 32);
         mainCard.append($("<p>").html("Temperature: " + temp + " &#8457")); 
         var humidity = response.main.humidity;
-        mainCard.append($("<p>").html("Humidity: " + humidity)); 
+        mainCard.append($("<p>").html("Humidity: " + humidity + " %")); 
         var windSpeed = response.wind.speed;
-        mainCard.append($("<p>").html("Wind Speed: " + windSpeed)); 
+        mainCard.append($("<p>").html("Wind Speed: " + windSpeed + " mph")); 
        
         var lat = response.coord.lat;
         var lon = response.coord.lon;
         
         // Call for the 5 day forecast.
         $.ajax({
-            url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=3e899aecdec7294393c72ce659cd2b48", 
+            url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=49fb27317373bb54f7d9243387af6df3", 
             method: "GET"
         // Display 5 separate columns in each day card.
         }).then(function (response) {
             for (i = 0; i < 5; i++) {
               
-                var newCard = $("<div>").attr("class", "col fiveDay bg-primary text-white rounded-lg p-2");
+                var newCard = $("<div>").attr("class", "col fiveDay bg-light text-black rounded-lg p-2");
                 $("#weeklyForecast").append(newCard);
              
                 var myDate = new Date(response.list[i * 8].dt * 1000);
@@ -86,8 +86,39 @@ function getData() {
        
                 var humidity = response.list[i * 8].main.humidity;
  
-                newCard.append($("<p>").html("Humidity: " + humidity));
+                newCard.append($("<p>").html("Humidity: " + humidity + " % "));
+
+                var windSpeed = response.list[i * 8].wind.speed;
+ 
+                newCard.append($("<p>").html("Wind speed: " + windSpeed + "mph"));
             }
         })
     })
 };
+
+
+// Find history and display on the left side of the screen.
+$("#search").click(function() {
+    city = $("#city").val().trim();
+    getData();
+    var checkArray = searchHistory.includes(city);
+    if (checkArray == true) {
+        return
+    }
+    else {
+        searchHistory.push(city);
+        localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+        var cityListButton = $("<a>").attr({
+           
+            class: "list-group-item list-group-item-action",
+            href: "#"
+        });
+        cityListButton.text(city);
+        $(".list-group").append(cityListButton);
+    };
+});
+
+$(".list-group-item").click(function() {
+    city = $(this).text();
+    getData();
+});
