6cd8ba2cbff3931cfefb9ffdb3799f0ed5014665
db <jacboysent@gmail.com>
2022-09-22 22:23:14 -0500

    js for displaying weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,63 +1,93 @@
-var getCityInfo = "http://api.openweathermap.org/geo/1.0/direct?q=london&limit=1&appid=3e899aecdec7294393c72ce659cd2b48";
-var displayCityName = document.querySelector("#display-city");
-// Search function //
-var displayMain = function() {
-    var searchedCity = "#city".val().trim();
-    
+// List for history.
+var searchHistory = [];
+// Returns the local storage search history back to user.
+function getItems() {
+    var storedCities = JSON.parse(localStorage.getItem("searchHistory"));
+    if (storedCities !== null) {
+        searchHistory = storedCities;
+    };
+    for (i = 0; i < searchHistory.length; i++) {
+        if (i == 8) {
+            break;
+          }
+
+        cityListButton = $("<a>").attr({
+            class: "list-group-item list-group-item-action",
+            href: "#"
+        });
+        
+        cityListButton.text(searchHistory[i]);
+        $(".list-group").append(cityListButton);
+    }
 };
 
+
 var city;
-var cityInfo = [];
-
-var getLatLong = function() {
-    fetch(getCityInfo)
-        .then((response) => response.json())
-        .then(function(response) {
-            var lat = response[0].lat;
-            var lon = response[0].lon;   
-            
-            var latLon = {
-                latitude: lat,
-                longitude: lon
-        }
-            localStorage.setItem("city", JSON.stringify(latLon));
-    })
-}
-
-  var getWeatherData = function() {
-    var getInfo = localStorage.getItem("city");
-    var getLatLon = JSON.parse(getInfo);
-    var infoArray = [(getLatLon)];
-
-    var cityURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + infoArray[0].latitude + "&lon=" + infoArray[0].longitude + "&appid=3e899aecdec7294393c72ce659cd2b48"
-    fetch(cityURL)
-        .then((response) => response.json())
-        .then(function(response) {
-            var temp = response.list[0].main.temp
-            var wind = response.list[0].wind.speed
-            var humidity = response.list[0].main.humidity
-            var city = response.city.name
-
-            var weatherInfo = {
-                name: city,
-                temprature: temp,
-                wind: wind,
-                humidity: humidity
-            }
-
-            localStorage.setItem("city", JSON.stringify(weatherInfo));
-            console.log(weatherInfo);
-        })    
-    }
-
-    var displayWeatherData = function () {
+var mainCard = $(".card-body");
+// Run getItems.
+getItems();
+// Main card.
+function getData() { 
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=3e899aecdec7294393c72ce659cd2b48" 
+    mainCard.empty();
+    $("#weeklyForecast").empty();
+    // Request the API.
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+        // Response function.
+    }).then(function (response) {
+        var date = moment().format(" MM/DD/YYYY");
         
-           var getName = localStorage.getItem("city");
-           var getNameArr = (getName.name);
-           (displayCityName).innerHTML = getNameArr;
-    }
+        var iconCode = response.weather[0].icon;
+    
+        var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
 
-getLatLong();
-getWeatherData();
-displayWeatherData();
+        var name = $("<h3>").html(city + date);
+ 
+        mainCard.prepend(name);
+   
+        mainCard.append($("<img>").attr("src", iconURL));
+      
+        var temp = Math.round((response.main.temp - 273.15) * 1.80 + 32);
+        mainCard.append($("<p>").html("Temperature: " + temp + " &#8457")); 
+        var humidity = response.main.humidity;
+        mainCard.append($("<p>").html("Humidity: " + humidity)); 
+        var windSpeed = response.wind.speed;
+        mainCard.append($("<p>").html("Wind Speed: " + windSpeed)); 
+       
+        var lat = response.coord.lat;
+        var lon = response.coord.lon;
+        
+        // Call for the 5 day forecast.
+        $.ajax({
+            url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=3e899aecdec7294393c72ce659cd2b48", 
+            method: "GET"
+        // Display 5 separate columns in each day card.
+        }).then(function (response) {
+            for (i = 0; i < 5; i++) {
+              
+                var newCard = $("<div>").attr("class", "col fiveDay bg-primary text-white rounded-lg p-2");
+                $("#weeklyForecast").append(newCard);
+             
+                var myDate = new Date(response.list[i * 8].dt * 1000);
+   
+                newCard.append($("<h4>").html(myDate.toLocaleDateString()));
+    
+                var iconCode = response.list[i * 8].weather[0].icon;
+  
+                var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
 
+                newCard.append($("<img>").attr("src", iconURL));
+   
+                var temp = Math.round((response.list[i * 8].main.temp - 273.15) * 1.80 + 32);
+     
+                newCard.append($("<p>").html("Temp: " + temp + " &#8457")); 
+       
+                var humidity = response.list[i * 8].main.humidity;
+ 
+                newCard.append($("<p>").html("Humidity: " + humidity));
+            }
+        })
+    })
+};
